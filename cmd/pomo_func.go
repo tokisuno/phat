b87ebeae80fa555d239bc8dde7f3e2ffd14aefca
tokisuno <lucaswmei@gmail.com>
2024-06-08 12:44:36 -0400

    coloured output

--- cmd/pomo_func.go
+++ cmd/pomo_func.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/fatih/color"
 	"github.com/gen2brain/beeep"
 )
 
 func startPomo() {
     // TODO: read these ints from config file
-    work_duration := 60
+    work_duration := 10
     break_duration := 10
 
     num_pomos := 3
 
     fmt.Println(work_duration)
-    for i := 0; i < num_pomos; i++ {
+    for i := 1; i <= num_pomos; i++ {
         fmt.Println("Starting session...")
         startSession(work_duration, "Session", i, num_pomos)
         notifyUser()
@@ -27,14 +28,38 @@ func startPomo() {
 }
 
 func startSession(duration int, state string, num int, denom int) {
-    duration *= 60
+    //duration *= 60
+    startingTime := duration
+
     for duration >= 0 {
         ClearScreen()
-        fmt.Printf("%s Timer:%v/%v >> %.2d:%.2d\r", state, num+1, denom, duration/60, duration%60)
+        colouredOutput(duration, startingTime, state, num, denom)
         time.Sleep(1 * time.Second)
         duration--
     }
+    ClearScreen()
     fmt.Printf("%v over!", state)
+}
+
+func colouredOutput(duration int, startingTime int, state string, num int, denom int) {
+    sessionText := color.New(color.FgWhite).Add(color.Underline)
+    durationText := color.New(color.FgHiBlack)
+
+    sessionText.Printf("%s Timer: %v/%v\n", state, num, denom)
+     
+    if duration <= startingTime/2 {
+        durationText = color.New(color.FgHiWhite)
+    } 
+
+    if duration < 30 {
+        durationText = color.New(color.FgHiRed)
+    }
+
+    if duration <= 5 {
+        durationText = color.New(color.FgHiYellow)
+    }
+
+    durationText.Printf("✨ %.2d:%.2d ✨\n\r", duration/60, duration%60)
 }
 
 func notifyUser() {
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/tokisuno/phat
 go 1.22.4
 
 require (
-	github.com/faiface/beep v1.1.0
+	github.com/fatih/color v1.17.0
 	github.com/gen2brain/beeep v0.0.0-20240516210008-9c006672e7f4
 	github.com/spf13/cobra v1.8.0
 )
@@ -11,14 +11,11 @@ require (
 require (
 	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
-	github.com/hajimehoshi/oto v0.7.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
-	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
-	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
-	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
